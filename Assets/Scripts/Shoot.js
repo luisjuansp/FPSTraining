6eed4569de5c903b00a31942275957f597d9758b
Luis Sanchez <luisjuansp@gmail.com>
2016-04-01 15:34:05 -0600

    Git Init

---
+++ Assets/Scripts/Shoot.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var Bala : Rigidbody;
+var speed : int;
+
+function Start () {
+
+}
+
+function Update () {
+	if ( Input.GetButtonDown("Fire1")) {
+		var clone = Instantiate(Bala, transform.position, transform.rotation);
+		clone.velocity = transform.TransformDirection(Vector3.up * speed);
+		Destroy (clone.gameObject, 1);
+  	}
+}
